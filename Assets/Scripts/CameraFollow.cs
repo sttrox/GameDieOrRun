3e6963fc943d17d37b04ebe0621be0e49b84a899
Denis Gankov <dgankov@ntpcentr.com>
2020-10-15 10:42:53 +0300

    Corrected script  the camera which following  for target

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,24 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
 
-    public float posDamp = 2; //2
-    public float rotDamp = 3; //3
-    public float height = 2; //2
-    public float distance = 5; //5
+    public Vector3 offset;
+    public float smooth = 5.0f;
 
-    private Vector3 targetPos;
-
-    // Update is called once per frame
     void Update()
     {
-        targetPos = target.position - target.forward * distance + target.up * height;
-        transform.position = Vector3.Lerp(transform.position, targetPos, posDamp * Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotDamp * Time.deltaTime);
+        transform.position =
+            Vector3.Lerp(transform.position, target.transform.position + offset, smooth * Time.deltaTime);
     }
 }
